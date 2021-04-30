8ef066b3e04208153582254e101efc1d12dc9cd4
dashahello <st.daryaa@gmail.com>
2021-04-30 17:29:33 +0300

    + comment example

--- src/helpers/randomNumberBetween.js
+++ src/helpers/randomNumberBetween.js
@@ -1,3 +1,9 @@
+/**
+ * 
+ * @param {number} min 
+ * @param {number} max 
+ * @returns {number}
+ */
 export default function randomNumberBetween(min, max) {
   return Math.random() * (max - min) + min;
 }
